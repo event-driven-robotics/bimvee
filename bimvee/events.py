35c8380e6d6421e91b5dcbe0c68a901dd562d857
Sim Bamford <simbamford@gmail.com>
2020-11-04 11:39:24 +0100

    refractoryPeriod filter added to events.py

--- bimvee/events.py
+++ bimvee/events.py
@@ -17,9 +17,11 @@ Basic manipulations specific to event streams
 """
 
 import numpy as np
+from tqdm import trange
 
 from bimvee.plotDvsContrast import getEventImage
 from bimvee.split import selectByBool
+
 '''
 removes events from pixels whose number of events is more than n * std above 
 mean, where n is the 'threshold kwarg, with default value 3
@@ -53,4 +55,41 @@ def removeHotPixels(inDict, **kwargs):
     eventAddrs = events['x'] * dimY + events['y']
     toKeep = np.logical_not(np.isin(eventAddrs, addrsToRemove))
     return selectByBool(events, toKeep)
-    
+
+''' 
+Iterates event by event, using a prevTs array to keep track of each pixel. 
+From a brief trial, dissecting into different arrays, 
+doing the filter and then merging again is quite a lot slower than this. 
+'''
+def refractoryPeriod(inDict, refractoryPeriod=0.001, **kwargs):
+    # boilerplate to get down to dvs container
+    if isinstance(inDict, list):
+        for inDictSingle in inDict:
+            removeHotPixels(inDictSingle, **kwargs)
+        return
+    if not isinstance(inDict, dict):
+        return
+    if 'ts' not in inDict:
+        for key in inDict.keys():
+            removeHotPixels(inDict[key], **kwargs)
+        return
+    # From this point onwards, it's a data-type container
+    if 'pol' not in inDict:
+        return
+    # From this point onwards, it's a dvs container
+    events = inDict
+    ts = events['ts']
+    x = events['y']
+    y = events['x']
+    numEvents = len(ts)
+    maxX = x.max()
+    maxY = y.max()
+    prevTs = np.zeros((maxY+1, maxX+1))
+    toKeep = np.ones((numEvents), dtype=np.bool)
+    for idx in trange(numEvents, leave=True, position=0):
+        if ts[idx] >= prevTs[y[idx], x[idx]] + refractoryPeriod: 
+            prevTs[y[idx], x[idx]] = ts[idx]
+        else:
+            toKeep[idx] = False
+    outDict = selectByBool(inDict, toKeep)
+    return outDict
