6434c0396f1d7484da67eda37745ebfa7b80ffb6
simbamford <simbamford@gmail.com>
2021-03-11 13:35:09 +0100

    changes to description.

--- bimvee/samplesToEvents.py
+++ bimvee/samplesToEvents.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Copyright (C) 2021 Event-driven Perception for Robotics
-Authors: Sim Bamford
-         Simon Muller-Cleve
+Authors: Simon Muller-Cleve
+         Sim Bamford
+         
 This program is free software: you can redistribute it and/or modify it under 
 the terms of the GNU General Public License as published by the Free Software 
 Foundation, either version 3 of the License, or (at your option) any later version.
--- examples/examplesSamplesToEvents.py
+++ examples/examplesSamplesToEvents.py
@@ -2,7 +2,6 @@
 """
 Copyright (C) 2021 Event-driven Perception for Robotics
 Authors: Sim Bamford
-         Simon Muller-Cleve
 This program is free software: you can redistribute it and/or modify it under 
 the terms of the GNU General Public License as published by the Free Software 
 Foundation, either version 3 of the License, or (at your option) any later version.
