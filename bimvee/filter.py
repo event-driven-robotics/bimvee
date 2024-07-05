c154984871ed8817fd7f5cf5e0ae68e66d0b3f45
simbamford <simbamford@gmail.com>
2024-07-05 15:02:53 +0200

    File has a more descriptive header, in local style, with authors, license etc.

--- bimvee/filter.py
+++ bimvee/filter.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  5 14:23:08 2024
+'''
+Copyright (C) 2024 Event-driven Perception for Robotics
+Authors: Sim Bamford, Mohammadreza Koolani
+This program is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with 
+this program. If not, see <https://www.gnu.org/licenses/>.
 
-@author: sbamford
-"""
+Intended as part of bimvee (Batch Import, Manipulation, Visualisation and Export of Events etc)
 
-"""
-also known as a salt-and-pepper filter,
+filter_spatiotemporal:
+Also known as a salt-and-pepper filter,
 lets an event pass if there has been a previous event 
 within a certain spatio-temporal window.
 """
+
+import numpy as np
+
 def filter_spatiotemporal(events, time_window=0.05, neighbourhood=1):
     xs = events['x']
     ys = events['y']
