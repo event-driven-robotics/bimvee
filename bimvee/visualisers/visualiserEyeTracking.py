cd5d4f3ef07254363880875e5752fb1a44f44364
Massimiliano Iacono <massimiliano.iacono@iit.it>
2024-10-01 17:34:59 +0200

    choose between linear and cubic interpolation based on distance

--- bimvee/visualisers/visualiserEyeTracking.py
+++ bimvee/visualisers/visualiserEyeTracking.py
@@ -29,11 +29,15 @@ these out for visualisation as an overlay.
 """
 
 import numpy as np
+from scipy.interpolate import interp1d
 
 # Local imports
 from .visualiserBase import Visualiser
 
-
+def radian_difference(rad1, rad2):
+    diff = rad1 - rad2
+    # Normalize the difference to be within [-π, π]
+    return (diff + np.pi) % (2 * np.pi) - np.pi
 class VisualiserEyeTracking(Visualiser):
 
     data_type = 'eyeTracking'
@@ -46,9 +50,9 @@ class VisualiserEyeTracking(Visualiser):
             if np.abs(self._data['ts'][idx] - time) > timeWindow:
                 if not kwargs.get('interpolate'):
                     return None
-                data_to_interpolate = {k: self.get_data()[k][idx-1:idx + 1] for k in self.get_data().keys() if hasattr(self.get_data()[k], '__len__')}
-                assert data_to_interpolate['ts'][0] < time < data_to_interpolate['ts'][1]
-                ratio = (time - data_to_interpolate['ts'][0]) / (data_to_interpolate['ts'][1] - data_to_interpolate['ts'][0])
+                data_to_interpolate = {k: self.get_data()[k][max(0, idx-2):idx + 2] for k in self.get_data().keys() if hasattr(self.get_data()[k], '__len__')}
+                if not(data_to_interpolate['ts'][0] < time < data_to_interpolate['ts'][-1]):
+                    return None
                 out_dict = {}
                 for x in data_to_interpolate:
                     val = data_to_interpolate[x]
@@ -56,7 +60,19 @@ class VisualiserEyeTracking(Visualiser):
                         out_dict[x] = val[0] and val[1]
                         continue
                     try:
-                        out_dict[x] = val[0] + ratio * (val[1] - val[0])
+                        if x == 'eyeball_theta' or x == 'eyeball_phi':
+                            ang_dist = abs(radian_difference(val[0], val[-1]))
+                            is_far_enough = ang_dist > 0.05
+                        elif x == 'eyeball_radius':
+                            is_far_enough = abs(val[0] - val[-1]) > 10
+                        elif x == 'eyeball_x' or x == 'eyeball_y':
+                            is_far_enough = abs(val[0] - val[-1]) > 5
+                        interp_kind = 'cubic' if is_far_enough else 'linear'
+                        try:
+                            cubic_interp = interp1d(data_to_interpolate['ts'], val, kind=interp_kind)
+                        except ValueError:
+                            cubic_interp = interp1d(data_to_interpolate['ts'], val, kind='linear')
+                        out_dict[x] = cubic_interp(time)
                     except TypeError:
                         continue
                 out_dict['interpolated'] = True
