fea6dc0ff4a69262e7d12a11be7e3ed1adb3542a
Massimiliano Iacono <massimiliano.iacono@iit.it>
2025-01-29 17:58:20 +0100

    Checking mismatch between images and timestamps and truncating accordingly

--- bimvee/importers/ImporterFrames.py
+++ bimvee/importers/ImporterFrames.py
@@ -2,12 +2,21 @@ from .ImporterBase import ImporterBase
 import numpy as np
 import imageio
 import os
+import logging
 class ImporterFrames(ImporterBase):
     
     def _do_indexing(self):
         self._file_stream.seek(0)
-        self._timestamps = np.loadtxt(self._file_stream)
         self._image_list = sorted([x for x in os.listdir(self._containing_dir_name) if os.path.splitext(x)[-1] in ['.jpg', '.jpeg', '.png']])
+        self._timestamps = np.loadtxt(self._file_stream)
+        if len(self._timestamps) != len(self._image_list):
+            if len(self._timestamps) > len(self._image_list):
+                logging.warning('More timestamps than images, truncating timestamps. Please check your data for missing images.')
+                self._timestamps = self._timestamps[:len(self._image_list)]
+            else:
+                logging.warning('More images than timestamps, truncating images. Please check your data for missing timestamps.')
+                self._image_list = self._image_list[:len(self._timestamps)]
+
 
     def get_data_at_time(self, time, time_window=None, **kwargs):
         data_idx = self.get_idx_at_time(time)
